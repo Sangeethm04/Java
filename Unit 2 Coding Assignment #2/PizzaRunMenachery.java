3c5a1c0466162eda37e7d397e59f6be0656470d8
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-10-17 17:27:57 -0400

    Update PizzaRunMenachery.java

--- Unit 2 Coding Assignment #2/PizzaRunMenachery.java
+++ Unit 2 Coding Assignment #2/PizzaRunMenachery.java
@@ -2,10 +2,15 @@
 // Exercise 2.3
 // Pizza Creation
 
+
+/*This wouldn't make sense if somebody only 7 pizzas for example where it would not say there is a full pizza so it would not 
+count it but I added 7 in case there was 9 slices and made it an int so it's always a whole number*/
+
+
 public class PizzaRunMenachery
 {
    // instance (class) variables
-   private double costPerPizza;
+   private double costPerPizza;//cost per pizza slice
    private int slicesPerPizza = 8;
    private int sum;  // total slices for the order
    private int numPizzas;  // total pizzas being ordered
@@ -13,9 +18,14 @@ public class PizzaRunMenachery
    private int extra;  // amount of slices that will be left over
    
    // constructor (creates a PizzaRun object)
-   public PizzaRun(double cost, int person1, int person2, int person3, int person4)
+   //person1,2,3,4 is amount of slices ate by each
+   public PizzaRunMenachery(double cost, int person1, int person2, int person3, int person4)
    {
-       
+
+       sum = person1 + person2 + person3 + person4;
+    numPizzas = (int)((sum + 7)/slicesPerPizza);
+    totalCost = cost * numPizzas;
+    extra = (numPizzas * slicesPerPizza) - (sum);     
     }
    
    //Accessor Method
@@ -42,10 +52,10 @@ public class PizzaRunMenachery
       /* Create two objects and print the number of pizzas for the order, the cost for each order,
          and the number of extra slices that will be left over once everyone eats their slices.
       */
-      PizzaRunMenachery pizza1 = new PizzaRunMenachery(35.0, 5, 5, 5, 5);
-      PizzaRunMenachery pizza2 = new PizzaRunMenachery(35.0, 5, 5, 5, 2);
-      System.out.println(pizza1.getPizzas() + pizza1.getCost());
-      System.out.println(pizza2.getPizzas() + pizza2.getCost());
+      PizzaRunMenachery pizza1 = new PizzaRunMenachery(35.0, 8, 8, 8, 7);
+      PizzaRunMenachery pizza2 = new PizzaRunMenachery(50.0, 17, 8, 8, 8);
+      System.out.println("Pizza #1 - \n Total Pizza - " + pizza1.getPizzas() + " \n Total Cost - " + pizza1.getCost() + "\n Extra Slices - " + pizza1.getExtra());
+      System.out.println("Pizza #2 - \n Total Pizza - " + pizza2.getPizzas() + " \n Total Cost - " + pizza2.getCost() + "\n Extra Slices - " + pizza2.getExtra());
    }
 }
 
