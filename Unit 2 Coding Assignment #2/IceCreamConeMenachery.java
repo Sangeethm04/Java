a742d770010d79f521dde2666d6e9788a267c486
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-10-16 23:23:36 -0400

    Update IceCreamConeMenachery.java

--- Unit 2 Coding Assignment #2/IceCreamConeMenachery.java
+++ Unit 2 Coding Assignment #2/IceCreamConeMenachery.java
@@ -1,8 +1,15 @@
 public class IceCreamConeMenachery {
+
+        public double height;
+        public double radius;
+        public double surfaceArea;  
+        public double volume; 
+
+        public Cone(height, radius) {
+
+        }
+
     public static void main(String[] args) {
-        String test = "  ONETWOTHREEFOURFIVESSAASD  ";
-        String smalls = test.trim();
-        String half = smalls.substring((smalls.length()/4) , (smalls.length() - smalls.length()/4));
-        System.out.println(half);
+        
     }
 }
