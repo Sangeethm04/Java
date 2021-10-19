0f133d1d2c687ed9afbf19509a8c688fabcf0e9a
menachery20231223 <90471512+menachery20231223@users.noreply.github.com>
2021-10-19 13:44:46 -0400

    ev

--- Unit 2 Coding Assignment #2/DigitReverseMenachery.java
+++ Unit 2 Coding Assignment #2/DigitReverseMenachery.java
@@ -3,27 +3,27 @@
 // Use var to reverse and output each number one by one
 
 public class DigitReverseMenachery {
-    public String num1 = "123123";
+    public String num1 = "12312";
 
     public String digit() {
-       String individual = num1.substring(1,2);
+       String individual = num1.substring(0,1);
         return individual;
     }
     public String digit2() {
-       String individual = num1.substring(2,3);
+       String individual = num1.substring(1,2);
         return individual;
     }
     public String digit3() {
-       String individual = num1.substring(3,4);
+       String individual = num1.substring(2,3);
         return individual;
     }
     public String digit4() {
        
-       String individual = num1.substring(4,5);
+       String individual = num1.substring(3,4);
         return individual;
     }
     public String digit5() {
-       String individual = num1.substring(5);
+       String individual = num1.substring(4);
         return individual;
     }
     public static void main(String[] args) {
