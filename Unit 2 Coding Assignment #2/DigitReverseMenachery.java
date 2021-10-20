c5afb70861dcf81eb98dd711d59c0a07b47a6a11
menachery20231223 <90471512+menachery20231223@users.noreply.github.com>
2021-10-20 11:36:04 -0400

    Update DigitReverseMenachery.java

--- Unit 2 Coding Assignment #2/DigitReverseMenachery.java
+++ Unit 2 Coding Assignment #2/DigitReverseMenachery.java
@@ -2,27 +2,26 @@
 // Exercise 2.6
 // Use var to reverse and output each number one by one
 
-public class DigitReverseMenachery {
+public class Main {
     private int num1;
     private int num2;
     private int num3;
-    private int num4;
-    private int num5;
-    private int num6;
-    private int num7;
+
 
     public int digit(int num2) {
-        num1 = num2;
-        num3 = num1 % 10;
-        num4 = num3 % 10;
-        num5 = num4 % 10;
-        num6 = num5 % 10;
-      return num3;
+    num1 = num2 % 10; // 5
+    num2 = num2 / 10;  //1234
+    System.out.println(num1); // 5
+    return num2; //1234
     }
 
     public static void main(String[] args) {
-        DigitReverseMenachery nums = new DigitReverseMenachery();
-        System.out.println(nums.digit(12345));
+        Main nums = new Main();
+        int number = nums.digit(12345);
+        number = nums.digit(number); //1234
+        number = nums.digit(number);
+        number = nums.digit(number);
+        number = nums.digit(number);
 
     }
 }
