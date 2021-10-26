c2819dc000ecea0408b1d27519a7676a4042d023
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-10-26 17:48:57 -0400

    Create test2.java

---
+++ Random/test2.java
@@ -0,0 +1,10 @@
+public class test2 {
+    public static void main(String[] args) {
+        int first = (int)(Math.random() * 9);
+    int second = (int)(Math.random() * 9);
+    int third = (int)(Math.random() * 9);
+    int fourth = (int)(Math.random() * 9);
+    System.out.println("Let’s play the lottery game! " + first + " - " + second + " - " + third + " - " + fourth);
+    }
+    
+}
