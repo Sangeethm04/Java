805d09238264b54e683014854a73996709f67662
menachery20231223 <90471512+menachery20231223@users.noreply.github.com>
2021-11-08 14:20:54 -0500

    Create Boolean_3_5.java

---
+++ Random/Boolean_3_5.java
@@ -0,0 +1,10 @@
+public class Boolean_3_5 {
+   public static void main(String[] args) {
+      int x = 5;
+if (x == 5)
+if (x == 4)
+System.out.println("x is not 5");
+else 
+System.out.println("x is not 4");
+}
+}
