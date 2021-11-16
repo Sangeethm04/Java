f6cb54ce664f4adbaa7f0786f825191e67eebce9
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-11-16 15:45:24 -0500

    Update test.java

--- Random/test.java
+++ Random/test.java
@@ -1,12 +1,8 @@
 public class test {
     public static void main(String[] args) {
-        int x = 10;
-          if(x < 7) 
-            if(x < 4)
-              System.out.print("Hello");
-             else 
-              System.out.print("Hola");
-              System.out.print("Bye");
+
+
+      
       }
         }
     
