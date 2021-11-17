ce13832a67da1cd6db0c9f8995deacfab59b8d3c
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-11-16 20:49:42 -0500

    Update test.java

--- Random/test.java
+++ Random/test.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
+      
+Scanner scan = new Scanner(System.in);
 
+System.out.println("Enter Username");
+
+String username = scan.nextLine();
+
+System.out.println("Username is " + username);
 
       
       }
