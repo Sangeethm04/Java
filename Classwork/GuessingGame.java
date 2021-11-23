0ffdfe60928247e93195d4914a91948c947f217f
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-11-23 16:48:31 -0500

    Create GuessingGame.java

---
+++ Classwork/GuessingGame.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class GuessingGame 
+{
+  public static void main(String[] args) 
+  {
+    Scanner scan = new Scanner(System.in);
+   // Choose a random number from 0-100 
+ int rand = (int)(Math.random() * 100);
+   // Ask the user to guess a number from 0 to 100 
+   // Get the first guess using scan.nextInt();
+ int guess = scan.nextInt();
+
+
+   // Loop while the guess does not equal the random number,
+while (guess != rand) 
+     // If the guess is less than the random number, print out "Too low!"
+     {
+        if (guess < rand)
+        {
+            System.out.println("Too low!");
+        }
+   
+      // If the guess is greater than the random number, print out "Too high!"
+    if (guess > rand) {
+        System.out.println("Too hight");
+    }
+      // Get a new guess (save it into the same variable)
+    guess = scan.nextInt();
+
+  
+ }
+// Print out something like "You got it!"
+    System.out.println("You got it!");
+  }
+}
