d471ebd53b7035fd057012c8f9a862ea1e9ecf9a
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-12-06 05:42:51 -0500

    ev

--- Random/test.java
+++ Random/test.java
@@ -1,17 +1,6 @@
-import java.util.Scanner;
-
 public class test {
     public static void main(String[] args) {
-      
-Scanner scan = new Scanner(System.in);
 
-System.out.println("Enter Username");
-
-String username = scan.nextLine();
-
-System.out.println("Username is " + username);
-
-      
       }
         }
     
--- Random/test2.java
+++ Random/test2.java
@@ -1,10 +1,24 @@
-public class test2 {
-    public static void main(String[] args) {
-        int first = (int)(Math.random() * 9);
-    int second = (int)(Math.random() * 9);
-    int third = (int)(Math.random() * 9);
-    int fourth = (int)(Math.random() * 9);
-    System.out.println("Let’s play the lottery game! " + first + " - " + second + " - " + third + " - " + fourth);
-    }
-    
+import java.util.*;
+import java.awt.*;
+
+public class test2
+{
+  public static void main(String[] args)
+  {
+      World world = new World(400,400);
+      Turtle yertle = new Turtle(world);
+      yertle.penUp();  // move a little to the left
+      yertle.moveTo(100,200);
+      yertle.penDown();
+      yertle.setColor(Color.blue);
+
+      // Add your loop here!
+      
+      for(int n = 5; n <= 4; n++) {
+      yertle.forward(100);
+      yertle.turn(72);
+      }
+
+      world.show(true);
+  }
 }
