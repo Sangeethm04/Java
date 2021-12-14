ef22286481b9f589085b2e21e39615e5866d19af
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2021-12-13 21:30:54 -0500

    ev

---
+++ Homework/loopanal.java
@@ -0,0 +1,11 @@
+public class loopanal {
+   public static void main(String[] args) {
+       int i =4, j =8;
+       while(i<=j && j!= 0) {
+           i = i + 1;
+           j = j-1;
+
+       }
+       System.out.println( i + " " + j);
+   } 
+}
---
+++ Homework/rand45.java
@@ -0,0 +1,12 @@
+public class rand45 {
+    public static void main(String[] args) {
+        for (int i = 3; i < 8; i++)
+        {
+            for (int y = 1; y < 5; y++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
