65eb0ab229208bc0c11a7a611be357f0340e012d
menachery20231223 <90471512+menachery20231223@users.noreply.github.com>
2021-12-17 13:04:18 -0500

    Update FunNumberMenachery.java

--- Unit 4 Coding Assignment/FunNumberMenachery.java
+++ Unit 4 Coding Assignment/FunNumberMenachery.java
@@ -1,5 +1,94 @@
 public class FunNumberMenachery {
+
+	private int original;
+	
+	/* constructs a FunNumber with value num
+	*/
+	public FunNumber(int num)
+	{
+   		original = num;
+	}
+ 
+	
+	/* counts the digits in this FunNumber value
+	 * @return the number of the digits
+	 */
+	public int numDigits()
+	{
+   		// insert code
+	}
+ 
+ 
+	/* sums the digits in this FunNumber value
+	 * @return the sum of the digits
+	 */
+	public int sumDigits()
+	{
+   		// insert code
+	}
+ 
+	
+	/* creates a sum of the integers that are less than or equal to this FunNumber value
+	 * @return the sum of the integers <= this FunNumber value
+	 */
+	public int sumInts()
+	{
+   	// insert code
+	}
+ 
+ 
+	/* determines the factorial of this FunNumber value
+	 * @return this FunNumber value's factorial
+	 */
+	public int findFactorial()
+	{
+   		// insert code
+	}
+	
+	/* prints all of the factors of this FunNumber value
+	 */
+	public void printFactors()
+	{
+   		// insert code
+	}
+ 
+	
+	/* determines if this FunNumber value is prime
+	 * @return true if this value is prime, false otherwise
+	 */
+ 	public boolean isPrime()
+	{
+   		// insert code
+	}
+ 
+	/* returns true if this FunNumber value is a "perfect" number 
+	 * (a perfect number is a number that has a sum of proper factors equal to the value
+ *  of this number. 6 is a perfect number because 6 = 1 + 2 + 3.)
+	 * @ return true if this FunNumber value is a perfect number, false otherwise.
+	 */
+    public boolean isPerfect()
+	{
+   		// insert code
+	}
+	
+ 	/* returns a number that is the reverse of this FunNumber
+ 	 * @return reverse of this FunNumber
+ 	 */
+	public int reverseNum()
+    	{
+    		// insert code
+	}
+
+	/* returns the value of this FunNumber
+	 * @return value of this FunNumber
+	 */
+	public int getValue()
+	{
+   		return original;
+	}
+
     public static void main(String[] args) {
-        
+
+
     }
-}
+    }
