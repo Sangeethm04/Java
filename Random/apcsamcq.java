deeaf8f9847b24151a674c898ee47b585430d56b
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-01-05 17:55:43 -0500

    fix bugs

--- Random/apcsamcq.java
+++ Random/apcsamcq.java
@@ -1,5 +1,12 @@
 public class apcsamcq {
    public static void main(String[] args) {
+  int n = 2;
+  int a = 0;
+  while (n>0) {
+     a += n % 10;
+     n /= 10;
+  }
+  System.out.println(a);
 
 }
 }
