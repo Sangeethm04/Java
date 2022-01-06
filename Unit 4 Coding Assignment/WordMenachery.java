cde7c4145dfa94a556af0bcaaee2fa3036ad9b28
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-01-05 22:55:57 -0500

    Update WordMenachery.java

--- Unit 4 Coding Assignment/WordMenachery.java
+++ Unit 4 Coding Assignment/WordMenachery.java
@@ -48,22 +48,31 @@ public class WordMenachery {
 	 * Alternate method to determine if word is a palindrome
 	 * 
 	 * @return true if word is a palindrome, false otherwise
+	 *  This version of isPalindrome will check pairs of letters. Each pair will consist of a letter from the beginning of the string and the corresponding place letter from the end of the string.  isPalindrome2 will return true if each pair has identical letters, false otherwise. Be efficient in writing this algorithm.
+
 	 * */
 	  public boolean isPalindrome2()
 	  {
-	    String reverse = "";
-	    for (int i = original.length() - 1; i >= 0; i--)
-	    {
-	      reverse += original.charAt(i);
-	    }
-	    return reverse.equalsIgnoreCase(original);
+		  int i = 0;
+		  int j = original.length() - 1;
+		  while (i < j)
+		  {
+			  if (original.charAt(i) != original.charAt(j))
+			  {
+				  return false;
+			  }
+			  i++;
+			  j--;
+		  }
+		  return true;
 	  }
+	
 	 
 
 	public static void main(String[] args) {
-		WordMenachery uno = new WordMenachery("Rotor");
+		WordMenachery uno = new WordMenachery("hannah");
 		System.out.println("First String: " + uno.isPalindrome2());
-		WordMenachery dos = new WordMenachery("Hello");
+		WordMenachery dos = new WordMenachery("rotor");
 		System.out.println("Second String: " + dos.isPalindrome2());
 	}
 }
