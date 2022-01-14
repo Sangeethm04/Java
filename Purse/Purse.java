20d9ab076133a66bb1e074d5ed9f22c8e9157d8d
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-01-14 15:08:39 -0500

    ev

---
+++ Purse/Purse.java
@@ -0,0 +1,36 @@
+public class Purse {
+    private static final double QUARTER_VALUE = 0.25;
+    private static final double DIME_VALUE = 0.1;
+    private static final double NICKEL_VALUE = 0.05;
+
+    // Define other fields needed 
+    private int numQuarters;
+    private int numDimes;
+    private int numNickels;
+
+
+    // Include a default constructor that creates an empty purse.
+    public Purse() {
+        numQuarters = 0;
+        numDimes = 0;
+        numNickels = 0;
+    }
+    // Include a method addQuarters that adds quarters to the purse.
+    public void addQuarters(int numberOfQuarters) {
+        
+    }
+
+    // Include a method addDimes that adds dimes to the purse.
+    public void addDimes(int numberOfDimes) {
+
+    }
+    // Include a method addNickels that adds nickels to the purse.
+    public void addNickels(int numberOfNickels) {
+
+    }
+    /* Include a method getTotal that returns the total value of 
+ the coins in the purse. */
+    public double getTotal() {
+        return 
+    }
+} //end of Purse class
