bc56d0b7cfb43e024d4333ed9712282fd2566503
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-01-24 12:55:47 -0500

    ev

--- Chapt5/BankAccountTesterMenachery.java
+++ Chapt5/BankAccountTesterMenachery.java
@@ -3,6 +3,8 @@
  * @author Sangeeth Menachery
  * @since 1/24/2022
  * This program is a fake bank account program.
+ * Preconditions: the name, account number, and balance must be valid.
+ * Postconditions: the name, account number, and balance are set to the values passed in.
  */
 public class BankAccountTesterMenachery {
     /**
@@ -15,9 +17,9 @@ public class BankAccountTesterMenachery {
         BankAccountMenachery bank2 = new BankAccountMenachery("Bob", "12345", 100.0);
         bank2.deposit(500.0);
         bank2.withdrawal(50.0);
-        bank2.getName();
-        bank2.getAccountNumber();
-        bank2.getBalance();
-        bank2.toString();
+        System.out.println(bank2.getName());
+        System.out.println(bank2.getAccountNumber());
+        System.out.println(bank2.getBalance());
+        System.out.println(bank2.toString());
     }
 }
