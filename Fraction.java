c834068e4d132430bd92924cefd845febac88492
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-02-01 20:02:52 -0500

    ev

---
+++ Fraction.java
@@ -0,0 +1,27 @@
+    /** Class Fraction */
+class Fraction {
+    //  instance variables
+    private int numerator;
+    private int denominator;
+
+    // constructor: set instance variables to default values
+    public Fraction() {
+        int d = 1;
+        numerator = d;
+        denominator = d;
+    }
+
+    // constructor: set instance variables to init parameters
+    public Fraction(int initNumerator, int initDenominator) {
+        numerator = initNumerator;
+        denominator = initDenominator;
+    }
+
+    public String toString() {
+        // if the denominator is 1, then just return the numerator
+        if (denominator == d) {
+            int newNumerator = 1;
+        }
+        return newNumerator + "/" + denominator;
+    }
+}
