5a1910632f63ad29407ef62c330b7287b0937b4b
menachery20231223 <90471512+menachery20231223@users.noreply.github.com>
2022-03-08 13:59:04 -0500

    ev

---
+++ Unit 6 Coding Assignment/ArrayAssignmentAnswersTesterMenachery.java
@@ -0,0 +1,12 @@
+// public class ArrayAssignmentAnswersTesterMenachery {
+//     public static void main(String[] args) {
+//         ArrayAssignmentMenachery a = new ArrayAssignmentMenachery();
+//         System.out.println(a.findNumber(1));
+//         System.out.println(a.findMax());
+//         System.out.println(a.findMin());
+//         System.out.println(a.findAvergage());
+//         a.swap(0, 1);
+//         a.reverse();
+//         a.numericOrder();
+//     }
+// }
