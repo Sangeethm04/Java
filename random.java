5e68c779f73230f25b96f47ee0551212330f6948
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-03-07 20:24:39 -0500

    ev

--- random.java
+++ random.java
@@ -1,13 +1,61 @@
-/* Write a block of code to initialize an array with 10 randomly generated integers between 1 and 20 and display them. */
-public class random {
-    public static void main(String[] args) {
-        int[] random = new int[10];
-        for (int i = 0; i < random.length; i++) {
-            random[i] = (int) (Math.random() * 20) + 1;
+import java.util.*;
+
+ public class random
+ {
+     private ArrayList<WordPair> allPairs;
+
+     public random(String[] words)
+     {
+         // WRITE YOUR CODE HERE
+         // initialize allPairs to an empty ArrayList of WordPair objects
+        ArrayList<WordPair> allPairs = new ArrayList<WordPair>();
+         // nested loops through the words array to add each pair to allPairs
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                allPairs.add(new WordPair(words[i], words[j]));
+            }
         }
-        for (int i = 0; i < random.length; i++) {
-            System.out.println(random[i]);
-        }
-        System.out.println("helo");
-    }
-}
+
+
+     }
+
+     public int numMatches()
+     {
+         //Write the code for the second part described below
+         return 0;
+     }
+
+     public String toString() {
+         return allPairs.toString();
+     }
+
+
+     public static void main(String[] args)
+     {
+         String[] words = {"Hi", "there", "Tyler", "Sam"};
+         random list = new random(words);
+         System.out.println(list);
+         // For second part below, uncomment this test:
+         //System.out.println("The number of matched pairs is: " + list.numMatches());
+     }
+ }
+
+ class WordPair {
+     private String word1;
+     private String word2;
+
+     public WordPair(String w1, String w2) {
+         word1 = w1;
+         word2 = w2;
+     }
+     public String getFirst() {
+         return word1;
+     }
+     public String getSecond() {
+         return word2;
+     }
+     public String toString() {
+         return "(" + word1 + ", " + word2 + ")";
+     }
+ }
+ 
