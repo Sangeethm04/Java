5332b852f65de43889a22167ee51c9acb45ebf3d
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-03-25 08:03:17 -0400

    Create SearchMenachery.java

---
+++ SearchMenachery.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class SearchMenachery {
+    ArrayList < Integer > nums = new ArrayList < Integer > ();
+    
+    public static void main(String[] args) {
+
+    }
+}
