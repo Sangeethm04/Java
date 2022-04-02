343801f512f48b5c203d24a6aea05b7ea4f26dbd
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-04-02 16:56:24 -0400

    ev

--- HourlyWorkerMenachery.java
+++ HourlyWorkerMenachery.java
@@ -1,42 +1,33 @@
-// Your subclass should contain two static fields that keep track of how many employees are receiving overtime pay and how much overtime pay is being given total from all employees.
-
-// Your subclass should contain a computePay method, two static accessor methods, and a toString method.  In the computePay method, if the amount of hours worked is less than or equal to 40, call on the superclass computePay method.  If the amount of hours worked is over 40, time-and-a-half overtime should be calculated.
-
-// Add to your tester file by creating 3 HourlyWorkers.  Test all methods with each object.
-
-
 public class HourlyWorkerMenachery extends WorkerMenachery {
-    private static int numemployees = 0;
-    private static int overtimePayTotal = 0;
+    private static int overtimeCount = 0;
+    private static double totalOvertimePay = 0;
 
     public HourlyWorkerMenachery(String firstName, String lastName, double salaryRate) {
         super(firstName, lastName, salaryRate);
-        //TODO Auto-generated constructor stub
     }
 
-    public void computePay(int hours) {
-        if (hours <= 40) {
-            super.computePay(hours);
+    public static int getOvertimeCount() {
+        return overtimeCount;
+    }
+
+    public static double getOvertimePay() {
+        return totalOvertimePay;
+    }
+    
+    public double computePay(int hoursWorked) {
+        if (hoursWorked <= 40) {
+            return super.computePay(hoursWorked);
         } else {
-            double overtime = hours - 40;
-            double overtimePay = overtime * (super.SalaryRate * 1.5);
-            setPay(getSalaryRate() * 40 + overtimePay);
-            overtimePayTotal += overtimePay;
-            numemployees++;
+            overtimeCount++;
+            totalOvertimePay += (super.computePay(40) + (super.computePay(hoursWorked - 40) * 1.5));
+            return (super.computePay(40) + (super.computePay(hoursWorked - 40) * 1.5));
         }
     }
 
-    public static int getNumemployees() {
-        return numemployees;
-    }
-
-    public static int getOvertimePayTotal() {
-        return overtimePayTotal;
-    }
-
     public String toString() {
-        return "Hourly Worker: " + getFirstName() + " " + getLastName() + " " + computePay();
+        return super.toString() + "Overtime Count: " + overtimeCount + " Overtime Pay: " + totalOvertimePay;
     }
+
 
     public static void main(String[] args) {
 
--- WorkerMenacheryTester.java
+++ WorkerMenacheryTester.java
@@ -18,5 +18,24 @@ public class WorkerMenacheryTester {
         System.out.println("Total Pay for worker 2: " + worker3.computePay(0));
         System.out.println(worker3);
 
+        System.out.println();
+
+        HourlyWorkerMenachery worker4 = new HourlyWorkerMenachery("Bob", "Smith", 5);
+        HourlyWorkerMenachery worker5 = new HourlyWorkerMenachery("Smith", "Lee", 5);
+        HourlyWorkerMenachery worker6 = new HourlyWorkerMenachery("Logan", "Fleming", 2);
+        System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getOvertimePay());
+        System.out.println("Total Pay for worker 4: " + worker4.computePay(2));
+        System.out.println(worker4);
+        System.out.println();
+        System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getOvertimePay());
+        System.out.println("Total Pay for worker 5: " + worker5.computePay(70));
+        System.out.println(worker5);
+        System.out.println();
+        System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getOvertimePay());
+        System.out.println("Total Pay for worker 6: " + worker6.computePay(50));
+        System.out.println(worker6);
     }
 }
