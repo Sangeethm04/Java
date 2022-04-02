0ed000c52af11115a1e5080b43536073096d6365
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-04-02 16:57:04 -0400

    ev

--- Chapt9/Person.java
+++ Chapt9/Person.java
@@ -1,50 +1,50 @@
-package Chapt9;
-public class Person {
-    private String name = null;
+// package Chapt9;
+// public class Person {
+//     private String name = null;
 
-    public Person(String theName) {
-        name = theName;
-    }
+//     public Person(String theName) {
+//         name = theName;
+//     }
 
-    public String getFood() {
-        return "Hamburger";
-    }
+//     public String getFood() {
+//         return "Hamburger";
+//     }
 
-    public static void main(String[] args) {
-        Person p = new Vegan("Javier");
-        System.out.println(p.getFood());
-    }
-}
+//     public static void main(String[] args) {
+//         Person p = new Vegan("Javier");
+//         System.out.println(p.getFood());
+//     }
+// }
 
-class Student extends Person {
-    private int id;
-    private static int nextId = 0;
+// class Student extends Person {
+//     private int id;
+//     private static int nextId = 0;
 
-    public Student(String theName) {
-        super(theName);
-        id = nextId;
-        nextId++;
-    }
+//     public Student(String theName) {
+//         super(theName);
+//         id = nextId;
+//         nextId++;
+//     }
 
-    public String getFood() {
-        String output = super.getFood();
-        return output + " and Pizza";
-    }
+//     public String getFood() {
+//         String output = super.getFood();
+//         return output + " and Pizza";
+//     }
 
-    public int getId() {
-        return this.id;
-    }
-    public void setId(int theId) {
-        this.id = theId;
-    }
-}
+//     public int getId() {
+//         return this.id;
+//     }
+//     public void setId(int theId) {
+//         this.id = theId;
+//     }
+// }
 
-class Vegan extends Student {
-    public Vegan(String theName) {
-        super(theName);
-    }
+// class Vegan extends Student {
+//     public Vegan(String theName) {
+//         super(theName);
+//     }
 
-    public String getFood() {
-        return "No " + super.getFood() + " but " + "Vegan food";
-    }
-}
+//     public String getFood() {
+//         return "No " + super.getFood() + " but " + "Vegan food";
+//     }
+// }
--- Chapt9/Pet.java
+++ Chapt9/Pet.java
@@ -1,59 +1,59 @@
-import Pet;
+// import Pet;
 
-package Chapt9;
-public class Pet {
-    private String name;
-    private String type;
+// package Chapt9;
+// public class Pet {
+//     private String name;
+//     private String type;
 
-    public Pet(String n, String t) {
-        name = n;
-        type = t;
-    }
-    public String getType() {
-        return type;
-    }
-    public String getName() {
-        return name;
-    }
+//     public Pet(String n, String t) {
+//         name = n;
+//         type = t;
+//     }
+//     public String getType() {
+//         return type;
+//     }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void speak() {
-        System.out.println("grr!");
-    }
-    public static void main(String[] args) {
-        Pet p = new Pet("Sammy", "hamster");
-        System.out.println(p.getType());
-        p.speak();
+//     public void speak() {
+//         System.out.println("grr!");
+//     }
+//     public static void main(String[] args) {
+//         Pet p = new Pet("Sammy", "hamster");
+//         System.out.println(p.getType());
+//         p.speak();
 
-        Dog d = new Dog("Fido");
-        System.out.println(d.getType());
-        d.speak();
-        Cat c = new Cat("Fluffy");
-        System.out.println(c.getType());
-        c.speak();
+//         Dog d = new Dog("Fido");
+//         System.out.println(d.getType());
+//         d.speak();
+//         Cat c = new Cat("Fluffy");
+//         System.out.println(c.getType());
+//         c.speak();
 
-    }
-}
+//     }
+// }
 
-// Complete the Dog class
- class Dog extends Pet {
+// // Complete the Dog class
+//  class Dog extends Pet {
 
-    public Dog(String n) {
-        super(n, "dog");
-    }
+//     public Dog(String n) {
+//         super(n, "dog");
+//     }
 
-    public void speak() {
-        System.out.println("Meow!");
-    }
+//     public void speak() {
+//         System.out.println("Meow!");
+//     }
 
-}
+// }
 
-// Add a Cat class
-class Cat extends Pet {
-    public Cat(String n) {
-        super(n, "cat");
-    }
-    public void speak() {
-        System.out.println("Woof!");
-    }
+// // Add a Cat class
+// class Cat extends Pet {
+//     public Cat(String n) {
+//         super(n, "cat");
+//     }
+//     public void speak() {
+//         System.out.println("Woof!");
+//     }
 
-}
+// }
