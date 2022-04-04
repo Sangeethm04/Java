27238b0fced6a7348b9337ccc20a8f70bb45591a
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-04-04 17:56:20 -0400

    Revert "ev"
    This reverts commit 849ff7ffcc0f48b47e23bb235a771f262c42e843.

--- Chapt9/Worker/Account.java
+++ Chapt9/Worker/Account.java
@@ -1,52 +1,52 @@
-// package Chapt9.Worker;
-// public class Account {
-//     private String name;
-//     private double balance;
+package Chapt9.Worker;
+public class Account {
+    private String name;
+    private double balance;
 
-//     public Account(String name, double balance) {
-//         this.name = name;
-//         this.balance = balance;
-//     }
+    public Account(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
 
-//     public String toString() {
-//         return name + ", " + balance;
-//     }
+    public String toString() {
+        return name + ", " + balance;
+    }
 
-//     public boolean equals(Object other) {
-//         Account otherAccount = (Account) other;
-//         return (this.balance == otherAccount.balance) &&
-//             this.name.equals(otherAccount.name);
-//     }
+    public boolean equals(Object other) {
+        Account otherAccount = (Account) other;
+        return (this.balance == otherAccount.balance) &&
+            this.name.equals(otherAccount.name);
+    }
 
-//     public static void main(String[] args) {
-//         Account acct1 = new Account("Armani Smith", 1500);
-//         System.out.println(acct1);
-//         SavingsAccount acct2 = new SavingsAccount("Dakota Jones", 1500, 4.5);
-//         SavingsAccount acct3 = new SavingsAccount("Dakota Jones", 1500, 4.5);
-//         System.out.println(acct2);
-//         System.out.println(acct2.equals(acct3));
+    public static void main(String[] args) {
+        Account acct1 = new Account("Armani Smith", 1500);
+        System.out.println(acct1);
+        SavingsAccount acct2 = new SavingsAccount("Dakota Jones", 1500, 4.5);
+        SavingsAccount acct3 = new SavingsAccount("Dakota Jones", 1500, 4.5);
+        System.out.println(acct2);
+        System.out.println(acct2.equals(acct3));
 
-//     }
-// }
-// /* Write the SavingsAccount class which inherits from Account
-//    and has an interest rate and a constructor, toString, and
-//    equals methods.
-// */
-// class SavingsAccount extends Account{
-//     private double interestRate;
+    }
+}
+/* Write the SavingsAccount class which inherits from Account
+   and has an interest rate and a constructor, toString, and
+   equals methods.
+*/
+class SavingsAccount extends Account{
+    private double interestRate;
 
-//     public SavingsAccount(double balance) {
-//         super(name, balance);
-//         this.interestRate = interestRate;
-//     }
+    public SavingsAccount(double balance) {
+        super(name, balance);
+        this.interestRate = interestRate;
+    }
 
-//     public String toString() {
-//         return super.toString() + ", " + interestRate;
-//     }
+    public String toString() {
+        return super.toString() + ", " + interestRate;
+    }
 
-//     public boolean equals(Object other) {
-//         SavingsAccount otherAccount = (SavingsAccount) other;
-//         return (this.interestRate == otherAccount.interestRate) &&
-//             super.equals(otherAccount);
-//     }
-// }
+    public boolean equals(Object other) {
+        SavingsAccount otherAccount = (SavingsAccount) other;
+        return (this.interestRate == otherAccount.interestRate) &&
+            super.equals(otherAccount);
+    }
+}
--- Chapt9/Worker/WorkerMenacheryTester.java
+++ Chapt9/Worker/WorkerMenacheryTester.java
@@ -1,43 +1,43 @@
-// import Chapt9.Worker.WorkerMenachery;
+import Chapt9.Worker.WorkerMenachery;
 
-// public class WorkerMenacheryTester {
-//     public static void main(String[] args) {
-//         WorkerMenachery worker1 = new WorkerMenachery("Bob", "Smith", 5);
-//         WorkerMenachery worker2 = new WorkerMenachery("Smith", "Lee", 5);
-//         WorkerMenachery worker3 = new WorkerMenachery("Logan", "Fleming", 2);
-//         System.out.println("First name of worker 1: " + worker1.getFirstName());
-//         System.out.println("Last name of worker 1: " + worker1.getLastName());
-//         System.out.println("Total Pay for worker 1: " + worker1.computePay(2));
-//         System.out.println(worker1);
-//         System.out.println();
-//         System.out.println("First name of worker 2: " + worker2.getFirstName());
-//         System.out.println("Last name of worker 2: " + worker2.getLastName());
-//         System.out.println("Total Pay for worker 2: " + worker2.computePay(6));
-//         System.out.println(worker2);
-//         System.out.println();
-//         System.out.println("First name of worker 3: " + worker3.getFirstName());
-//         System.out.println("Last name of worker 3: " + worker3.getLastName());
-//         System.out.println("Total Pay for worker 2: " + worker3.computePay(0));
-//         System.out.println(worker3);
+public class WorkerMenacheryTester {
+    public static void main(String[] args) {
+        WorkerMenachery worker1 = new WorkerMenachery("Bob", "Smith", 5);
+        WorkerMenachery worker2 = new WorkerMenachery("Smith", "Lee", 5);
+        WorkerMenachery worker3 = new WorkerMenachery("Logan", "Fleming", 2);
+        System.out.println("First name of worker 1: " + worker1.getFirstName());
+        System.out.println("Last name of worker 1: " + worker1.getLastName());
+        System.out.println("Total Pay for worker 1: " + worker1.computePay(2));
+        System.out.println(worker1);
+        System.out.println();
+        System.out.println("First name of worker 2: " + worker2.getFirstName());
+        System.out.println("Last name of worker 2: " + worker2.getLastName());
+        System.out.println("Total Pay for worker 2: " + worker2.computePay(6));
+        System.out.println(worker2);
+        System.out.println();
+        System.out.println("First name of worker 3: " + worker3.getFirstName());
+        System.out.println("Last name of worker 3: " + worker3.getLastName());
+        System.out.println("Total Pay for worker 2: " + worker3.computePay(0));
+        System.out.println(worker3);
 
-//         System.out.println();
+        System.out.println();
 
-//         HourlyWorkerMenachery worker4 = new HourlyWorkerMenachery("Bob", "Smith", 5);
-//         HourlyWorkerMenachery worker5 = new HourlyWorkerMenachery("Smith", "Lee", 5);
-//         HourlyWorkerMenachery worker6 = new HourlyWorkerMenachery("Logan", "Fleming", 2);
-//         System.out.println("How many employees are receiving overtime pay: " + HourlyWorkerMenachery.getOvertimeCount());
-//         System.out.println("Overtime pay is being given total: " + HourlyWorkerMenachery.getTotalOvertimePay());
-//         System.out.println("Total Pay for worker 4: " + worker4.computePay(2));
-//         System.out.println(worker4);
-//         System.out.println();
-//         System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
-//         System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getTotalOvertimePay());
-//         System.out.println("Total Pay for worker 5: " + worker5.computePay(70));
-//         System.out.println(worker5);
-//         System.out.println();
-//         System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
-//         System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getTotalOvertimePay());
-//         System.out.println("Total Pay for worker 6: " + worker6.computePay(50));
-//         System.out.println(worker6);
-//     }
-// }
+        HourlyWorkerMenachery worker4 = new HourlyWorkerMenachery("Bob", "Smith", 5);
+        HourlyWorkerMenachery worker5 = new HourlyWorkerMenachery("Smith", "Lee", 5);
+        HourlyWorkerMenachery worker6 = new HourlyWorkerMenachery("Logan", "Fleming", 2);
+        System.out.println("How many employees are receiving overtime pay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Overtime pay is being given total: " + HourlyWorkerMenachery.getTotalOvertimePay());
+        System.out.println("Total Pay for worker 4: " + worker4.computePay(2));
+        System.out.println(worker4);
+        System.out.println();
+        System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getTotalOvertimePay());
+        System.out.println("Total Pay for worker 5: " + worker5.computePay(70));
+        System.out.println(worker5);
+        System.out.println();
+        System.out.println("Total People getting overtimePay: " + HourlyWorkerMenachery.getOvertimeCount());
+        System.out.println("Total Overtime Pay given: " + HourlyWorkerMenachery.getTotalOvertimePay());
+        System.out.println("Total Pay for worker 6: " + worker6.computePay(50));
+        System.out.println(worker6);
+    }
+}
