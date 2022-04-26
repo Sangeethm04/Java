ef090814aac87bd82327552fe8c8325f55d8412f
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-04-26 17:28:17 -0400

    ev

--- random.java
+++ random.java
@@ -1,4 +1,6 @@
 public class random {
+   public double random = 0.12;
+
    public static void main(String[] args) {
       System.out.println("Hello World!");
    }
--- random2.java
+++ random2.java
@@ -1,6 +1,7 @@
-public class random2 {
-    public double random = 0.1;
+public class random2 extends random{
+    static public double random = 0.1;
     public static void main(String[] args) {
+        System.out.println("Hello World!" + random);
 
     }    
 }
