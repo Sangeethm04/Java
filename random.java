899e5cda918eba6ee27e3ce6b302e978e3179939
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2022-05-05 21:00:22 -0400

    ev

--- random.java
+++ random.java
@@ -1,5 +1,17 @@
 public class random {
+
+   public static boolean isNonZero(int[][] array2D, int r) {
+
+      for (int j = 0; j < array2D[0].length; j++) {
+         if (array2D[r - 1][j] == 0) {
+            return false;
+         }
+
+      }
+      return true;
+
+   }
    public static void main(String[] args) {
-      
+
    }
 }
--- random2.java
+++
@@ -1,7 +0,0 @@
-public class random2 extends random{
-    static private double random = 0.2;
-    public static void main(String[] args) {
-        System.out.println("Hello World!" + random);
-
-    }    
-}
